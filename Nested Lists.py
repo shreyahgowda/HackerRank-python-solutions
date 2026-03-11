6bc6496ec6a1f1ca42e2aead7ae4638e809ca6e9
shreyahgowda <shreyahgowda06@gmail.com>
2026-03-11 14:23:06 +0530

    Add files via upload

---
+++ Nested Lists.py
@@ -0,0 +1,15 @@
+if __name__ == '__main__':
+    students = []
+    
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        students.append([name, score])
+
+    scores = sorted(set([s[1] for s in students]))
+    second = scores[1]
+
+    result = [s[0] for s in students if s[1] == second]
+
+    for name in sorted(result):
+        print(name)
